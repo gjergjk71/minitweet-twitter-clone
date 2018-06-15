b963566730659403d63ecc67ee9dec1f52559141
gjergjk71 <gjergjk71@gmail.com>
2018-06-15 12:29:26 +0200

    Added LOGIN_URL and LOGIN_REDIRECT_URL

--- minitweet/settings.py
+++ minitweet/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = "/login"
+LOGIN_REDIRECT_URL = "/feed/"
 
 # Application definition
 
@@ -117,5 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATICFILES_DIRS = [
+    '/home/gjergj/Desktop/minitweet-twitter-clone/users/static/',
+]
 STATIC_URL = '/static/'
