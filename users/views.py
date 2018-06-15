ae187eaa97a8b4f2ac0cf87f6831dca3fb6d5535
gjergjk71 <gjergjk71@gmail.com>
2018-06-15 12:55:25 +0200

    Removed custom login view

--- users/views.py
+++ users/views.py
@@ -2,11 +2,3 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import login 
 
 # Create your views here.
-
-def custom_login(request):
-	current_user = request.user
-	if current_user.is_authenticated:
-		return redirect("/feed/")
-	else:
-		return login(request)
-
