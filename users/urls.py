69698678a5d5316634327c9e7458c000254c27f7
Gjergj Kadriu <gjergjk71@gmail.com>
2018-06-16 12:40:55 +0200

    <websitelink>/ redirects to /login now

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+	path('', views.redirectLogin,name="redirectLogin"),
     path("login", views.custom_login, name="login"),
     path("register", views.register, name="register"),
 ]
