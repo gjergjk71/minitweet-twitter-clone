871a17363f0a3fcbde13a68c2ecd04a754ecec0b
gjergjk71 <gjergjk71@gmail.com>
2018-06-17 18:07:37 +0200

    Created showProfile view

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from .models import Profile,Follow
 # Create your views here.
+
+def showProfile(request,username):
+	user = User.objects.get(username=username)
+	profile = Profile.objects.get(user=user)
+
+	user_followers = len(Follow.objects.filter(follower=user))
+	user_following = len(Follow.objects.filter(following=user))
+
+	context = {"user_info":user,"profile":profile,
+			"user_followers":user_followers,"user_following":user_following}
+
+	return render(request,"profile.html",context)
+
