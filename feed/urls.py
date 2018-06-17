2573672addd52e8847a5a0c502fe9c3a98cacca7
gjergjk71 <gjergjk71@gmail.com>
2018-06-17 13:40:19 +0200

    Mapped "createTweet" path with tweets.views.createTweet

--- feed/urls.py
+++ feed/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
-
+from tweets.views import createTweet
 urlpatterns = [
     path("feed", views.feed,name="feed"),
+    path("feed/createTweet/<redirectTo>", createTweet,name="createTweet")
 ]
