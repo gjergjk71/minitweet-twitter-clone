91033dc45d6e37d57ab80043fc1b143c4ac20452
gjergjk71 <gjergjk71@gmail.com>
2018-06-17 16:29:37 +0200

    Created Profile and Follow models

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User,on_delete=models.CASCADE)
+	birthday = models.DateTimeField()
+	education = models.CharField(max_length=100)
+	def __str__(self):
+		return str(self.user)
+class Follow(models.Model):
+	follower = models.ForeignKey(User,on_delete=models.CASCADE,related_name="follower")
+	following = models.ForeignKey(User,on_delete=models.CASCADE,related_name="following")
+	def __str__(self):
+		return "{} started following {}".format(self.follower.username,self.following.username)
