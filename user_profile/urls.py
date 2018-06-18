1aec84f3d4de1389fba53e5fda4028df609d5aae
gjergjk71 <gjergjk71@gmail.com>
2018-06-18 14:54:29 +0200

    Removed <redirectTo> on createTweet path

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from tweets.views import createTweet
 
 urlpatterns = [
 	path("profile/<username>",views.showProfile,name="showProfile"),
 	path("follow/<ID>",views.followUser,name="followUser"),
 	path("unfollow/<ID>",views.unfollowUser,name="unfollowUser"),
-
+    path("profile/createTweet/", createTweet,name="createTweet")
 ]
