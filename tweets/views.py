4e79792d790210837eff0ab937d0c5137374e7ac
gjergjk71 <gjergjk71@gmail.com>
2018-06-18 14:53:39 +0200

    createTweet() Does the same thing more effectively

--- tweets/views.py
+++ tweets/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from .models import Tweet
 # Create your views here.
 
-def createTweet(request,redirectTo):
+def createTweet(request):
 	if request.method == "POST":
 		current_user = request.user
 		text = request.POST.get("tweetText")
+		nextPage = request.POST.get("next")
 		tweet = Tweet.objects.create(user=current_user,text=text)
-		return redirect(redirectTo)
+		return redirect(nextPage)
 	else:
-		return redirect(redirectTo)
+		return redirect(nextPage)
