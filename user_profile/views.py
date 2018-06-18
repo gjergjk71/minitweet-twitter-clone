fed784968082b9f2ad055e980f2f4096264b01f6
gjergjk71 <gjergjk71@gmail.com>
2018-06-18 11:50:21 +0200

    Created unfollowUser view

--- user_profile/views.py
+++ user_profile/views.py
@@ -6,11 +6,13 @@ from .models import Profile,Follow
 def showProfile(request,username):
 	user = User.objects.get(username=username)
 	profile = Profile.objects.get(user=user)
-
+	unfollow_user = False
 	user_followers = len(Follow.objects.filter(follower=user))
 	user_following = len(Follow.objects.filter(following=user))
 
-	context = {"user_info":user,"profile":profile,
+	if Follow.objects.filter(follower=request.user,following=user):
+		unfollow_user = True
+	context = {"user_info":user,"profile":profile,"unfollow_user":unfollow_user,
 			"user_followers":user_followers,"user_following":user_following}
 
 	return render(request,"profile.html",context)
@@ -23,4 +25,12 @@ def followUser(request,ID):
 	if not Follow.objects.filter(**follow_args):
 		Follow.objects.create(**follow_args)
 
+	return redirect("/profile/{}".format(follow_args["following"].username))
+
+def unfollowUser(request,ID):
+	follow_args = {
+		"follower":request.user,
+		"following":User.objects.get(pk=ID)
+	}
+	Follow.objects.filter(**follow_args).delete()
 	return redirect("/profile/{}".format(follow_args["following"].username))
