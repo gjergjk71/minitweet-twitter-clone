0212ebea62e25a65b9f766d3e78f629ffbbde137
gjergjk71 <gjergjk71@gmail.com>
2018-06-19 13:24:17 +0200

    Registered Comment,Like(Tweet/Comment) to admin

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Tweet,HashTag
+from .models import Tweet,HashTag,Comment,LikeTweet,LikeComment
 # Register your models here.
 
 admin.site.register(Tweet)
 admin.site.register(HashTag)
+admin.site.register(Comment)
+admin.site.register(LikeTweet)
+admin.site.register(LikeComment)
