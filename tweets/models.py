2127a114e6a680733e1ac27f67243025449e4f19
gjergjk71 <gjergjk71@gmail.com>
2018-06-19 13:23:26 +0200

    Created Comment,LikeTweet,LikeComment models

--- tweets/models.py
+++ tweets/models.py
@@ -10,6 +10,26 @@ class Tweet(models.Model):
 	def __str__(self):
 		return self.text[:250]
 
+class Comment(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	tweet = models.ForeignKey(Tweet,on_delete=models.CASCADE)
+	text = models.TextField(max_length=1000)
+	created_date = models.DateTimeField(auto_now_add=True)
+	def __str__(self):
+		return self.text[:250]
+
+class LikeTweet(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	tweet = models.ForeignKey(Tweet,on_delete=models.CASCADE)
+	def __str__(self):
+		return "{} - {}".format(self.user,self.tweet)
+
+class LikeComment(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	comment = models.ForeignKey(Comment,on_delete=models.CASCADE)
+	def __str__(self):
+		return "{} - {}".format(self.user,self.comment)
+
 class HashTag(models.Model):
 	name = models.CharField(max_length=100,unique=True)
 	tweets = models.ManyToManyField(Tweet)
