618471f347014daacae4fb59da289e6b878a41ce
gjergjk71 <gjergjk71@gmail.com>
2018-06-22 13:04:01 +0200

    Users can tweet on others profile now

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Tweet
+from django.contrib.auth.models import User
+from user_profile.models import Profile
 # Create your views here.
 
 def createTweet(request):
@@ -7,7 +9,14 @@ def createTweet(request):
 		current_user = request.user
 		text = request.POST.get("tweetText")
 		nextPage = request.POST.get("next")
-		tweet = Tweet.objects.create(user=current_user,text=text)
+		if nextPage == "/feed":
+			user_profile = Profile.objects.get(user=current_user)
+			tweet = Tweet.objects.create(profile=user_profile,user=current_user,text=text)
+		elif "/profile/" in nextPage:
+			username = nextPage.split("/")[-1]
+			user_profile = User.objects.get(username=username)
+			current_profile = Profile.objects.get(user=user_profile)
+			tweet = Tweet.objects.create(profile=current_profile,user=current_user,text=text)
 		return redirect(nextPage)
 	else:
 		return redirect(nextPage)
