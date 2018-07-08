efd8da3e8c529076fcef2dc5e22262d1f1e3085a
gjergjk71 <gjergjk71@gmail.com>
2018-07-08 16:38:31 +0200

    Made birthday field unrequired

--- user_profile/models.py
+++ user_profile/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
 	user = models.OneToOneField(User,on_delete=models.CASCADE)
 	description = models.TextField(max_length=250,blank=True)
-	birthday = models.DateTimeField()
+	birthday = models.DateTimeField(blank=True)
 	education = models.CharField(max_length=100,blank=True)
 	def __str__(self):
 		return str(self.user)
