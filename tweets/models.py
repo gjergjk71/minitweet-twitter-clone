1e9c18b479832803532b504e902d0124fb29a203
Rishi Banerjee <rishieric91@gmail.com>
2018-07-17 17:42:07 +0700

    Added Chat templates and Views

--- tweets/models.py
+++ tweets/models.py
@@ -2,38 +2,48 @@ from django.db import models
 from django.contrib.auth.models import User
 from user_profile.models import Profile
 
+
 # Create your models here.
 
 class Tweet(models.Model):
-	profile = models.ForeignKey(Profile,on_delete=models.CASCADE, default="")
-	user = models.ForeignKey(User,on_delete=models.CASCADE)
-	text = models.TextField(max_length=1000)
-	created_date = models.DateTimeField(auto_now_add=True)
-	def __str__(self):
-		return self.text[:250]
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=1000)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:250]
+
 
 class Comment(models.Model):
-	user = models.ForeignKey(User,on_delete=models.CASCADE)
-	tweet = models.ForeignKey(Tweet,on_delete=models.CASCADE)
-	text = models.TextField(max_length=1000)
-	created_date = models.DateTimeField(auto_now_add=True)
-	def __str__(self):
-		return self.text[:250]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    text = models.TextField(max_length=1000)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:250]
+
 
 class LikeTweet(models.Model):
-	user = models.ForeignKey(User,on_delete=models.CASCADE)
-	tweet = models.ForeignKey(Tweet,on_delete=models.CASCADE)
-	def __str__(self):
-		return "{} - {}".format(self.user,self.tweet)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {}".format(self.user, self.tweet)
+
 
 class LikeComment(models.Model):
-	user = models.ForeignKey(User,on_delete=models.CASCADE)
-	comment = models.ForeignKey(Comment,on_delete=models.CASCADE)
-	def __str__(self):
-		return "{} - {}".format(self.user,self.comment)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} - {}".format(self.user, self.comment)
+
 
 class HashTag(models.Model):
-	name = models.CharField(max_length=100,unique=True)
-	tweets = models.ManyToManyField(Tweet)
-	def __str__(self):
-		return self.name
+    name = models.CharField(max_length=100, unique=True)
+    tweets = models.ManyToManyField(Tweet)
+
+    def __str__(self):
+        return self.name
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 # Create your models here.
 
 class Profile(models.Model):
-	user = models.OneToOneField(User,on_delete=models.CASCADE)
-	description = models.TextField(max_length=250,blank=True)
-	birthday = models.DateTimeField(null=True,blank=True)
-	education = models.CharField(max_length=100,blank=True)
-	def __str__(self):
-		return str(self.user)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=250, blank=True)
+    birthday = models.DateTimeField(null=True, blank=True)
+    education = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+
 class Follow(models.Model):
-	follower = models.ForeignKey(User,on_delete=models.CASCADE,related_name="follower")
-	following = models.ForeignKey(User,on_delete=models.CASCADE,related_name="following")
-	def __str__(self):
-		return "{} started following {}".format(self.follower.username,self.following.username)
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+
+    def __str__(self):
+        return "{} started following {}".format(self.follower.username, self.following.username)
